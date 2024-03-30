803bba34bfdad2c5de647dc7939a77dd45977897
Daffa Shafwan <moch.chairullah@tiket.com>
2024-03-30 21:04:50 +0700

    add cors

--- routes/routes.go
+++ routes/routes.go
@@ -3,11 +3,19 @@ package routes
 import (
 	appHandler "cekzakat/handlers"
 
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/labstack/echo/v4"
 )
 
 func RegisterRoutes(e *echo.Echo) {
 	mainApp := e.Group("/cekzakat")
+
+	mainApp.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"http://localhost", "https://cekzakat.vercel.app"},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+		AllowMethods: []string{echo.GET, echo.POST, echo.PUT, echo.DELETE},
+	}))
+	
 	mainApp.POST("/hitung-zakat", appHandler.ZakatHandler)
 	mainApp.POST("/register", appHandler.RegisterHandler)
 	mainApp.POST("/login", appHandler.LoginHandler)
