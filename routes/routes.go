c055cd3b67b2ae33c8a555225d83f70f6aa8d3e3
fianrmdni <fianrmdni@gmail.com>
2024-03-30 14:00:47 +0700

    cek zakat mainApp register, login, logout

--- routes/routes.go
+++ routes/routes.go
@@ -9,5 +9,8 @@ import (
 func RegisterRoutes(e *echo.Echo) {
 	mainApp := e.Group("/cekzakat")
 	mainApp.POST("/hitung-zakat", appHandler.ZakatHandler)
+	mainApp.POST("/register", appHandler.RegisterHandler)
+	mainApp.POST("/login", appHandler.LoginHandler)
+	mainApp.POST("/login", appHandler.LogoutHandler)
 
 }
